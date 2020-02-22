fb4c8ea2b1464172915f5a3cf1e32a671ba66351
Michael Le <mnle529@gmail.com>
2020-02-21 23:02:02 -0800

    updated seed

--- seed.sql
+++ seed.sql
@@ -12,12 +12,12 @@ INSERT INTO
     role
     (title, salary, department_id)
 VALUES
-    ("Sales Lead", 100000, 1),
-    ("Salesperson", 80000, 1),
-    ("Lead Engineer", 150000, 2),
-    ("Software Engineer", 120000, 2),
-    ("Accountant", 125000, 3),
-    ("Account Manager", 140000, 3),
-    ("Legal Team Lead", 190000, 4),
-    ("Lawyer", 150000, 4);
+    ("Sales Lead", 90000, 1),
+    ("Salesperson", 70000, 1),
+    ("Lead Engineer", 125000, 2),
+    ("Engineer", 100000, 2),
+    ("Assistant Accountant", 80000, 3),
+    ("Accountant", 100000, 3),
+    ("Legal Senior Advisor", 150000, 4),
+    ("Lawyer", 120000, 4);
 
